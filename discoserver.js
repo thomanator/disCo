d3f9950f6a7636972b6ce39af6280cd97fd10116
unknown <vikramthomas93@gmail.com>
2015-07-12 15:42:03 +0530

    new commit

--- discoserver.js
+++ discoserver.js
@@ -4,55 +4,37 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var WELL = require('well-rng')
 var rng = new WELL();
-
-var hexVal,keyVal;
 var mongodb = require('mongodb');
-
 var MongoClient = mongodb.MongoClient;
-
 var url = 'mongodb://localhost:27017/kbase';
 
-var range = 50;
-
-app.get('/',function(req,res)
-{
-res.sendfile('discoclient.html');
+app.get('/',function(req,res) {      //for routing to discoclient.html
+  res.sendfile('discoclient1.html');
 })
 
-
-io.on('connection',function(socket){
-var rand = rng.randInt(0,1000);
-io.emit('random',rand);
-socket.on('hexval',function(msg,db)
-{
-hexVal = msg;
-console.log('key-value: '+rand+'Hexa-value '+hexVal);
-MongoClient.connect(url, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    
-    var hexval = db.collection('hexval');
-    var cursor = hexval.find({key:rand});
-    cursor.each(function (err, doc) {
+io.on('connection',function(socket) {
+  socket.on('blah',function(msg){
+    console.log(msg.a+','+msg.b);
+    MongoClient.connect(url, function (err, db) {
       if (err) {
-        console.log(err);
-      } else {
-        if(doc==null)
-        {
-           hexval.insert({key:rand,hexvalue:hexVal});
-        }
+        console.log('Unable to connect to the mongoDB server. Error:', err);
+      }
+      else {
+        var hexval = db.collection('new');
+        var cursor = hexval.find({key:msg.a});
+        cursor.each(function (err, doc) {
+          if (err) {
+            console.log(err);
+          } 
+          else {
+            if (doc==null) {
+            hexval.insert({key:msg.a,hexvalue:msg.b});  //inserting value only if the value for the key does not exist
+            }
+          }
+        });
       }
     });
-  
-   
-    
-  }
+  });
 });
-});
-
-});
-
 http.listen(3000);
-
-console.log('listening on 3000');
+console.log('listening on 3000')
